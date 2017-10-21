e429c75fc9c3efd3a509f26b04ee2ed27ba695f6
Lydia Phillips <Lydia@Lydias-MacBook-Air.local>
2017-10-21 13:49:17 -0400

    addition of preliminary color picker

--- js/form.js
+++ js/form.js
@@ -1,29 +1,63 @@
 
 
 
-
-
 function submitbutton() { 
 
 	console.log( "ready! 2" );
 
+	var cityimage = $( "#cityimage" ).val();
+	var cityname = $( "#cityname" ).val();
+	var textinput = $( "#textinput" ).val();
+	var reviewone = $( "#reviewone" ).val();
+	var reviewtwo = $( "#reviewtwo" ).val();
 
-	var firstname = $( "#firstname" ).val();
-	var lastname = $( "#lastname" ).val();
-	var companyname = $( "#companyname" ).val();
-	var domainname = $( "#domainname" ).val();
-	console.log(firstname, lastname, companyname, domainname);
+
+	var data = {
+		cityimage: cityimage, 
+		cityname: cityname, 
+		textinput: textinput, 
+		reviewone: reviewone, 
+		reviewtwo: reviewtwo
+	};
+
+	console.log(data);
 
 	$.ajax({
 		type:"POST",
-		url:"https://www.pushlaundry.com/test.php",
+		url:"https://www.pushlaundry.com/hack/test.php",
 		success: function(data) {
 			console.log(data);
 		},
+		data: data,
 		dataType: 'text',
 	});
 
 }
+
+
+var colorWell;
+var defaultColor = "#0000ff";
+
+window.addEventListener("load", startup, false);
+function startup() {
+  colorWell = document.querySelector("#colorWell");
+  colorWell.value = defaultColor;
+  colorWell.addEventListener("input", updateFirst, false);
+  colorWell.addEventListener("change", updateAll, false);
+  colorWell.select();
+}
+function updateFirst(event) {
+  var p = document.querySelector("p");
+
+  if (p) {
+    p.style.color = event.target.value;
+  }
+}function updateAll(event) {
+  document.querySelectorAll("p").forEach(function(p) {
+    p.style.color = event.target.value;
+  });
+}
+
 
 
 
@@ -34,5 +68,9 @@ $( document ).ready(function() {
 	$( "#submitbutton").click(submitbutton)
 
 });
+
+
+
+
 
 
